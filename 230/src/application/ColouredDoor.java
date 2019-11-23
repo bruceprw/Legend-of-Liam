ab226feb0c5b30ed8781bc7cdc3d84bda30fbfe9
andyshuo <57260106+andyshuo@users.noreply.github.com>
2019-11-23 17:21:31 +0000

    debug

--- 230/src/application/ColouredDoor.java
+++ 230/src/application/ColouredDoor.java
@@ -2,9 +2,12 @@ package application;
 
 public class ColouredDoor extends Cell {
 	private String colour;
+	private String greenPath = "Images\\Green.png";
 	
     public ColouredDoor(String colour) {
-        super(false, true, false, "", colouredKey);
+        super(false, true, false, "");
         this.colour = colour;
     }
+    
+    
 }
--- 230/src/application/TokenDoor.java
+++ 230/src/application/TokenDoor.java
@@ -21,4 +21,6 @@ public class TokenDoor extends Cell {
     {
     	return tokenNumToUnlockDoor;
     }
+    
+    
 }
